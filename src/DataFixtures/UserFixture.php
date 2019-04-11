d1b0766e9f531dbaa0a3848f4bbf3d3e6304d14f
Docker fake user <docker.fake.user@example.com>
2019-04-11 19:35:39 +0000

    making user fixtures

---
+++ src/DataFixtures/UserFixture.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class UserFixture extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+
+
+
+
+
+        $user = new User();
+        $user->setEmail(sprintf('spacebar1@example.com'));
+        $user->setUsername(sprintf('Name'));
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
