739df8dd8b40af19274c28c19e9384c836548c65
Docker fake user <docker.fake.user@example.com>
2019-04-14 07:17:41 +0000

    updated ProductFixture

--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -3,19 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Product;
-use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
-
-//class ProductFixtures extends Fixture
-//{
-//    public function load(ObjectManager $manager)
-//    {
-//        // $product = new Product();
-//        // $manager->persist($product);
-//
-//        $manager->flush();
-//    }
-//}
 
 class ProductFixtures extends BaseFixtures
 {
