4d2bc287b46e77a149c9da1feb2914013c30e25d
Roman Juchimcuk <romasprojects@gmail.com>
2019-05-04 13:16:25 +0300

    reacto paleidimas

---
+++ assets/js/testas.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Testas extends React.Component {
+    render() {
+        return(
+        <div>
+            <h1>Labas</h1>
+        </div>
+        );
+    }
+}
+
+export default Testas;
