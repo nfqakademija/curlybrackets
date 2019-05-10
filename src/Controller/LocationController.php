cb2e25bd11349ce502f3ab52613c5dfe2e3eba3c
“Marijus84” <marijus.romanovas@hotmail.com>
2019-05-10 20:51:01 +0300

    location set logic implement in backend

---
+++ src/Controller/LocationController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Location;
+use App\Form\LocationType;
+use App\Repository\LocationRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @Route("/location")
+ */
+class LocationController extends AbstractController
+{
+
+    /**
+     * @Route("/set", name="location_set", methods={"GET","POST"})
+     */
+    public function set(Request $request, UserInterface $user = null): Response
+    {
+        $location = new Location();
+        $form = $this->createForm(LocationType::class, $location);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($location);
+            $entityManager->flush();
+            $user->setLocation($location);
+            $this->getDoctrine()->getManager()->flush();
+            return $this->redirectToRoute('user_show', [
+                'id' => $user->getId(),
+            ]);
+        }
+
+        return $this->render('location/set.html.twig', [
+            'location' => $location,
+            'form' => $form->createView(),
+        ]);
+    }
+
+
+    /**
+     * @Route("/{id}/edit", name="location_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Location $location): Response
+    {
+        $form = $this->createForm(LocationType::class, $location);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('location_index', [
+                'id' => $location->getId(),
+            ]);
+        }
+
+        return $this->render('location/edit.html.twig', [
+            'location' => $location,
+            'form' => $form->createView(),
+        ]);
+    }
+
+}
