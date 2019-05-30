019dc5cebf3dcd418fdb3919a2b9ab7a9c02a2e0
“Marijus84” <marijus.romanovas@hotmail.com>
2019-05-30 13:54:35 +0300

    fix on easy admin bundle

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -170,7 +170,7 @@ class User implements UserInterface, Serializable
     public function getRoles(): array
     {
         $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
+        // guarantee every user at leaewst has ROLE_USER
         $roles[] = 'ROLE_USER';
 
         return array_unique($roles);
